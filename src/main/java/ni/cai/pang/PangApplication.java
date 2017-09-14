f51a7880f871cdb262ba14669a3fdeeca3cc8e10
TDKnight <412046232@qq.com>
2017-09-14 15:47:42 +0800

    init

---
+++ src/main/java/ni/cai/pang/PangApplication.java
@@ -0,0 +1,12 @@
+package ni.cai.pang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PangApplication.class, args);
+	}
+}
